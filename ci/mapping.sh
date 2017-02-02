0fd6e80efc1852255b469f6b1de15762da8c8a19
moonii <mhlee@crossent.com>
2017-02-02 15:08:57 +0900

    add ci/mapping.yml

---
+++ ci/mapping.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#set -e
+
+echo "mapping.sh called"
+
+echo "ls -o ci-git"
+ls ci-git
+
+
+echo "ls -o ci-repo==================="
+ls -o ci-repo
+echo "======================================="
+
